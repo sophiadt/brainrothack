8603e1a862fed0cd9404babea934486d19a70550
Sophia Don Tranho <sophiadnth@gmail.com>
2024-11-29 12:59:50 -0800

    create call page

---
+++ client/app/call/page.tsx
@@ -0,0 +1,9 @@
+export default function CallPage() {
+  return (
+    <main className="relative purple flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Call Page</h1>
+      </div>
+    </main>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import App from "../components/App";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -7,6 +8,11 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <h1>Rizz Your Alpha</h1>
         <Hero />
+        <Link href="/call">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Call
+          </button>
+        </Link>
         <App />
       </div>
     </main>
